b76844e3e118a9f4081e68a9262451ecb044deff
Qusai Akoud <qusaiakoud@gmail.com>
2020-03-27 17:09:35 -0400

    styles circles

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,50 @@
 import 'modernizr';
 
-// eslint-disable-next-line
-console.log('🐶');
+const canvas = document.getElementById('canvas');
+
+const w = 900;
+const h = (canvas.height = w);
+const ctx = canvas.getContext('2d');
+const scale = window.devicePixelRatio;
+let frame = 0;
+
+setup();
+
+function setup() {
+  canvas.width = w * scale;
+  canvas.height = h * scale;
+
+  canvas.style.width = `${w}px`;
+  canvas.style.height = `${h}px`;
+
+  ctx.scale(scale, scale);
+}
+
+function circ(x, y, rad, opacity) {
+  ctx.beginPath();
+  ctx.strokeStyle = `rgba(52, 152, 252, ${opacity})`;
+  ctx.lineWidth = 1;
+  ctx.arc(x, y, rad, 0, Math.PI * 2);
+  ctx.stroke();
+  ctx.closePath();
+}
+
+function offset(source, offset) {
+  return Math.floor(source / offset);
+}
+
+function animate() {
+  window.requestAnimationFrame(animate);
+  frame++;
+
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+  const circleOffset = offset(w / 2, 15);
+
+  for (let i = 0; i < circleOffset; i++) {
+    const radius = ((i * (w / circleOffset) + frame) % w) / 2;
+    const opacity = 1 - ((i * (w / circleOffset) + frame) % w) / 2 / (w / 2);
+    circ(w / 2, h / 2, radius, opacity);
+  }
+}
+animate();
